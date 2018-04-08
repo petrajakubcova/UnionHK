2efed0cdab80e996a993ce757911b1e1767a5241
sarahberglund <sarah.keen15@gmail.com>
2018-04-08 15:32:48 +0200

    DOB field and H2 added
    Added values to the Date of Birth to give more options in the dropdowns.
    Added h2 to each page to decsribe what it's about

--- Index.php
+++ Index.php
@@ -79,6 +79,8 @@
 					<input type="password" id="password" name="password" placeholder="Password"/>
 					<label for="password">Repeat your password: </label>
 					<input type="password" id="password" name="password" placeholder="Password"/>
+
+
 					<!-- Date of birth -->
 					<div data-role="fieldcontain">
 						<fieldset data-role="controlgroup" data-type="horizontal">
@@ -88,20 +90,125 @@
 							<select name="select-choice-month" id="select-choice-month">
 								<option>Month</option>
 								<option value="jan">January</option>
-								<!-- etc. -->
+								<option value="feb">February</option>
+								<option value="mar">March</option>
+								<option value="apr">April</option>
+								<option value="may">May</option>
+								<option value="jun">June</option>
+								<option value="jul">July</option>
+								<option value="aug">August</option>
+								<option value="sep">September</option>
+								<option value="oct">October</option>
+								<option value="nov">November</option>
+								<option value="dec">December</option>
 							</select>
 
 							<label for="select-choice-day">Day</label>
 							<select name="select-choice-day" id="select-choice-day">
 								<option>Day</option>
 								<option value="1">1</option>
+								<option value="2">2</option>
+								<option value="3">3</option>
+								<option value="4">4</option>
+								<option value="5">5</option>
+								<option value="6">6</option>
+								<option value="7">7</option>
+								<option value="8">8</option>
+								<option value="9">9</option>
+								<option value="10">10</option>
+								<option value="11">11</option>
+								<option value="12">12</option>
+								<option value="13">13</option>
+								<option value="14">14</option>
+								<option value="15">15</option>
+								<option value="16">16</option>
+								<option value="17">17</option>
+								<option value="18">18</option>
+								<option value="19">19</option>
+								<option value="20">20</option>
+								<option value="21">21</option>
+								<option value="22">22</option>
+								<option value="23">23</option>
+								<option value="24">24</option>
+								<option value="25">25</option>
+								<option value="26">26</option>
+								<option value="27">27</option>
+								<option value="28">28</option>
+								<option value="29">29</option>
+								<option value="30">30</option>
+								<option value="31">31</option>
 								<!-- etc. -->
 							</select>
 
 							<label for="select-choice-year">Year</label>
 							<select name="select-choice-year" id="select-choice-year">
 								<option>Year</option>
+								<option value="2020">2020</option>
+								<option value="2019">2019</option>
+								<option value="2018">2018</option>
+								<option value="2017">2017</option>
+								<option value="2016">2016</option>
+								<option value="2015">2015</option>
+								<option value="2014">2014</option>
+								<option value="2013">2013</option>
+								<option value="2012">2012</option>
 								<option value="2011">2011</option>
+								<option value="2010">2010</option>
+								<option value="2009">2009</option>
+								<option value="2008">2008</option>
+								<option value="2007">2007</option>
+								<option value="2006">2006</option>
+								<option value="2005">2005</option>
+								<option value="2004">2004</option>
+								<option value="2003">2003</option>
+								<option value="2002">2002</option>
+								<option value="2001">2001</option>
+								<option value="2000">2000</option>
+								<option value="1999">1999</option>
+								<option value="1998">1998</option>
+								<option value="1997">1997</option>
+								<option value="1996">1996</option>
+								<option value="1995">1995</option>
+								<option value="1994">1994</option>
+								<option value="1993">1993</option>
+								<option value="1992">1992</option>
+								<option value="1991">1991</option>
+								<option value="1990">1990</option>
+								<option value="1989">1989</option>
+								<option value="1988">1988</option>
+								<option value="1987">1987</option>
+								<option value="1986">1986</option>
+								<option value="1985">1985</option>
+								<option value="1984">1984</option>
+								<option value="1983">1983</option>
+								<option value="1982">1982</option>
+								<option value="1981">1981</option>
+								<option value="1980">1980</option>
+								<option value="1979">1979</option>
+								<option value="1978">1978</option>
+								<option value="1977">1977</option>
+								<option value="1976">1976</option>
+								<option value="1975">1975</option>
+								<option value="1974">1974</option>
+								<option value="1973">1973</option>
+								<option value="1972">1972</option>
+								<option value="1971">1971</option>
+								<option value="1970">1970</option>
+								<option value="1969">1969</option>
+								<option value="1968">1968</option>
+								<option value="1967">1967</option>
+								<option value="1966">1966</option>
+								<option value="1965">1965</option>
+								<option value="1964">1964</option>
+								<option value="1963">1963</option>
+								<option value="1962">1962</option>
+								<option value="1961">1961</option>
+								<option value="1960">1960</option>
+								<option value="1959">1959</option>
+								<option value="1958">1958</option>
+								<option value="1957">1957</option>
+								<option value="1956">1956</option>
+								<option value="1955">1955</option>
 								<!-- etc. -->
 							</select>
 						</fieldset>
@@ -136,7 +243,7 @@
 		</div>
 		<!-- content -->
 		<div data-role="main" class="ui-content">
-			<p>Page content</p>
+			<h2>Pay Slip Information</h2>
 			<div data-role="fieldcontain">
 				<div>
 					<!-- do we need to change any of these input types to numbers?-->
@@ -188,7 +295,7 @@
 		</div>
 		<!-- content -->
 		<div data-role="main" class="ui-content">
-			<p>Page content</p>
+			<h2>Salary Calculator</h2>
 			<div data-role="fieldcontain">
 				<div>
 					<label for="name">Job Role:</label>
@@ -222,6 +329,7 @@
 		</div>
 		<!-- content -->
 		<div data-role="main" class="ui-content">
+			<h2>HK and Jobpatruljen Information</h2>
 			<p>This is just a place holder for information from Jobpatruljen.dk and HK.dk</p>
 			<p>Maybe we could create a couple of boxes/ use some lorem ipsum to show a possible page design?</p>
 		</div>
@@ -233,7 +341,5 @@
 		</div>
 	</div>
 
-
 </body>
-
 </html>
